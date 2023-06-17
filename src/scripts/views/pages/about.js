0e252bec4373727c66cc32222077ce2d86be77df
ngurahchandraa <ngurahchandra@student.telkomuniversity.ac.id>
2023-06-17 19:47:26 +0800

    Update About Page and Adding AOS

--- src/scripts/views/pages/about.js
+++ src/scripts/views/pages/about.js
@@ -1,47 +1,84 @@
 const About = {
   async render() {
     return `
-      <div class="about-content">
-        <div class="text">
-          <h1>Manage Your Task!</h1>
-          <!-- Additional text content -->
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nemo, nobis dicta esse accusamus alias dolor blanditiis ut expedita fuga sit cupiditate corporis? Quam vero ex officia. Fugiat, sint voluptatem!. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Magnam quia, quas corporis amet maiores ad. Minus aperiam hic itaque consequatur! Cumque ipsum assumenda vitae? Ipsum velit animi ab facilis natus?
-          </p>
-        </div>
-        <div class="about-image">
-          <img src="img/checklist.png" alt="Image" />
-        </div>
-      </div>
+    <div class="left-2">
+    
+    <div class="text" data-aos="fade-up">
+      <ul class="feature">
+      <li><p id="make">Manage your task! Make it</p></li>
+        <li><p id="make">About uTask!</p></li>
+        
+      </ul>
+      <!-- Additional text content -->
+    </div>
+    
 
-      <div class="about-content-2">
-        <div class="text">
-          <h1>About uTask ?</h1>
-          <!-- Additional text content -->
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nemo, nobis dicta esse accusamus alias dolor blanditiis ut expedita fuga sit cupiditate corporis? Quam vero ex officia. Fugiat, sint voluptatem!. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Magnam quia, quas corporis amet maiores ad. Minus aperiam hic itaque consequatur! Cumque ipsum assumenda vitae? Ipsum velit animi ab facilis natus?
-          </p>
-        </div>
-        <div class="about-image-2">
-          <img src="img/about-check.png" alt="Image" />
-        </div>
-      </div>
+    <div class="sub-text" data-aos="fade-up" data-aos-delay="200">
+      <p>
+      <strong>uTask</strong> provides efficient and structured task<br>
+      management solutions using the help of the<br>
+      API from Google Calendar
+      </p>
+    </div>
+  </div>
 
-      <div class="about-content-3">
-        <div class="text">
-          <h1>Why uTask ?</h1>
-          <!-- Additional text content -->
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nemo, nobis dicta esse accusamus alias dolor blanditiis ut expedita fuga sit cupiditate corporis? Quam vero ex officia. Fugiat, sint voluptatem!. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Magnam quia, quas corporis amet maiores ad. Minus aperiam hic itaque consequatur! Cumque ipsum assumenda vitae? Ipsum velit animi ab facilis natus?
-          </p>
-        </div>
-        <div class="about-image-3">
-          <img src="img/about-ques.png" alt="Image" />
-        </div>
-      </div>
+  <div class="gclogo-img" data-aos="fade-up" data-aos-delay="400">
+    <img src="img/checklist.png" alt="Image" />
+  </div>
+
+
+
+  <div class="left-2">
+    
+    <div class="text" data-aos="fade-up">
+      <ul class="feature">
+        <li><p id="make">About UTask!</p></li>
+        
+      </ul>
+      <!-- Additional text content -->
+    </div>
+
+    <div class="sub-text" data-aos="fade-up" data-aos-delay="200">
+      <p>
+      <STRONG>uTask</STRONG> is a website for structuring or managing
+      tasks or assignments. This website will also
+      combine all email accounts owned by users so
+      that they become one place and are not
+      separated. We took this problem because we
+      ourselves had experienced difficulties in
+      managing SIB and other household tasks. What's
+      more, there are friends who take part-time jobs
+      which are difficult to divide the time.
+      </p>
+    </div>
+  </div>
+
+  <div class="gclogo-img" data-aos="fade-up" data-aos-delay="400">
+    <img src="img/about-check.png" alt="Image" />
+  </div>
+
+
+  <div class="left-2-about">
+    
+  <div class="text" data-aos="fade-up">
+    <ul class="feature">
+      <li><p id="make">Why uTask ?</p></li>
+      
+    </ul>
+    <!-- Additional text content -->
+  </div>
+
+  <div class="sub-text" data-aos="fade-up" data-aos-delay="200">
+    <p>1. uTask have to manage tasks and organize tasks by integrating email with Google Calendar.</p>
+    <p>2. Manage task management in an efficient and structured manner using the help of Google Calendar's API.</p>
+    <p>3. Insert a collection link with a reminder, so you don't forget when there is an assignment submission
+    deadline.</p>
+  </div>
+</div>
+
+<div class="gcalendar-img" data-aos="fade-up" data-aos-delay="400">
+  <img src="img/people.png" alt="Image" />
+</div>
     `;
   },
 
@@ -49,6 +86,8 @@ const About = {
     const element = document.querySelector("main");
     element.id = "";
     // Fungsi ini akan dipanggil setelah render()
+
+    AOS.init();
   },
 };
 
--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,45 +1,121 @@
 const Home = {
   async render() {
     return `
-      <div class="left">
-        <div class="text">
-          <ul class="feature">
-            <li><p id="make">Manage your task! Make it</p></li>
-            <li><h1 id="word">SCHEDULED!</h1></li>
-          </ul>
-          <!-- Additional text content -->
-        </div>
+    <div class="left">
+    
+    <div class="text" data-aos="fade-up">
+      <ul class="feature">
+        <li><p id="make">Manage your task! Make it</p></li>
+        <li><h1 id="word">SCHEDULED!</h1></li>
+      </ul>
+      <!-- Additional text content -->
+    </div>
 
-        <div class="sub-text">
-          <p>
-            <strong>uTask</strong> adalah sebuah website yang dibuat untuk
-            membantu pengguna dalam manajemen dan penataan tugas. Website ini
-            dirancang khusus untuk memenuhi kebutuhan mahasiswa dan individu
-            yang ingin mengatur tugas-tugas mereka dengan efektif. <br />
-            <strong>uTask</strong> bertujuan untuk meningkatkan produktivitas
-            pengguna dalam mengelola tugas-tugas mereka dan membantu mengatasi
-            kendala-kendala yang sering terjadi dalam manajemen tugas.
-          </p>
-          <button class="get-started">
-            <a href="#/dashboard">Get Started</a>
-          </button>
-        </div>
-      </div>
+    <div class="sub-text" data-aos="fade-up" data-aos-delay="200">
+      <p>
+        <strong>uTask</strong> adalah sebuah website yang dibuat untuk
+        membantu pengguna dalam manajemen dan penataan tugas. Website ini
+        dirancang khusus untuk memenuhi kebutuhan mahasiswa dan individu
+        yang ingin mengatur tugas-tugas mereka dengan efektif. <br />
+        <strong>uTask</strong> bertujuan untuk meningkatkan produktivitas
+        pengguna dalam mengelola tugas-tugas mereka dan membantu mengatasi
+        kendala-kendala yang sering terjadi dalam manajemen tugas.
+      </p>
+      <button class="get-started">
+        <a href="#/about">Get Started</a>
+      </button>
+    </div>
+  </div>
 
-      <div class="people-calendar-img">
-        <img src="img/homepage-main.png" alt="Image" />
-      </div>      
+  <div class="people-calendar-img" data-aos="fade-up" data-aos-delay="400">
+    <img src="img/homepage-main.png" alt="Image" />
+  </div>
+  
+
+
+  <div class="left-2">
+    
+    <div class="text" data-aos="fade-up">
+      <ul class="feature">
+        <li><p id="make">Deadline Reminders</p></li>
+        
+      </ul>
+      <!-- Additional text content -->
+    </div>
+    
+
+    <div class="sub-text" data-aos="fade-up" data-aos-delay="200">
+      <p>
+        <strong>We</strong> understand that life can get busy. That's why uTask provides smart reminders to keep you informed and on
+        top of your tasks. Whether it's an assignment submission deadline, an important meeting, or a personal goal,
+        our reminders will ensure you never miss a beat.
+      </p>
+    </div>
+  </div>
+
+  <div class="gclogo-img" data-aos="fade-up" data-aos-delay="400">
+    <img src="img/GC-logo.png" alt="Image" />
+  </div>
+
+
+
+  <div class="left-2">
+    
+    <div class="text" data-aos="fade-up">
+      <ul class="feature">
+        <li><p id="make">Get Connect with your Google Calendar!</p></li>
+        
+      </ul>
+      <!-- Additional text content -->
+    </div>
+
+    <div class="sub-text" data-aos="fade-up" data-aos-delay="200">
+      <p>
+      <STRONG>uTask</STRONG> seamlessly integrates with Google
+            Calendar, offering
+            a unified
+            platform for managing both your tasks and events. Effortlessly sync your tasks with your calendar, ensuring
+            a comprehensive view of your commitments. Never miss an important deadline or appointment again.
+      </p>
+    </div>
+  </div>
+
+  <div class="gcalendar-img" data-aos="fade-up" data-aos-delay="400">
+    <img src="img/gcalendar.png" alt="Image" />
+  </div>
+
+
+  <div class="left-2-last">
+    
+    <div class="text-last" data-aos="fade-up">
+      <ul class="feature-last">
+        <li><p>Experience uTask Today!</p></li>
+        
+      </ul>
+
+      <div class="sub-text-last" data-aos="fade-up" data-aos-delay="200">
+      <p>
+      Start your uTask journey now and unlock the full potential of your productivity.
+      </p>
+    </div>
+      <!-- Additional text content -->
+    </div>
+
+    <div class="people-img" data-aos="fade-up" data-aos-delay="400">
+    <img src="img/people.png" alt="Image" />
+  </div>
+
+  </div>
+
+
+
     `;
   },
 
   async afterRender() {
-
-
-    
-    // Fungsi ini akan dipanggil setelah render()
     const element = document.querySelector("main");
     element.id = "";
-    // Scrolling
+
     let prevScrollpos = window.pageYOffset;
     const navbar = document.getElementById("navbar");
 
@@ -63,8 +139,11 @@ const Home = {
       wordElement.innerText = words[currentIndex];
       currentIndex = (currentIndex + 1) % words.length;
     }
-
     setInterval(changeWord, 2000);
+    // Initialize AOS library
+    AOS.init();
+
+    // Scrolling and sliding code omitted for brevity
   },
 };
 
